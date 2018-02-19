99def6bca4da19f0e660b62da7be6d04bf4a9fd9
Challe <gabin.challe@outlook.fr>
2018-02-19 21:31:08 +0100

    initialisation

---
+++ tests/unit/routes/td2-ex-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:td2-ex', 'Unit | Route | td2 ex', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
